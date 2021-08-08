21bda8e5474583d1d28db6671bab0ad0e5f52605
mppatterson84 <mppatterson84@gmail.com>
2021-08-08 12:38:50 -0500

    pass onDelete prop to task component and assign to icon

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,10 @@
 import { FaTrash } from 'react-icons/fa';
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete }) => {
   return (
     <div className="task mt-1 p-2">
       <span className="delete">
-        <FaTrash />
+        <FaTrash onClick={() => onDelete(task.id)} />
       </span>
       <h4>{task.title}</h4>
       <p>{task.detail}</p>
