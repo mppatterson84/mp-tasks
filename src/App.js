75bbb8e2eca9543d57d494eef183704c2a4a1a89
mppatterson84 <mppatterson84@gmail.com>
2021-08-08 09:47:51 -0500

    add tasks component to app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
   return (
     <div className="app-container container d-flex justify-content-center">
       <div className="card p-2 align-self-center">
         <Header />
+        <Tasks />
       </div>
     </div>
   );
