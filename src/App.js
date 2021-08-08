bd08c33cc06c29bc1fd52a660e97c50370745ffe
mppatterson84 <mppatterson84@gmail.com>
2021-08-08 09:55:31 -0500

    move tasks array to app component and useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Finish Building Tasks API',
+      detail: 'Implement all the features for the API.',
+      completed: true,
+      created_at: '2021-08-07T08:15:26.401197-05:00',
+      due_by: '2021-08-08T08:15:26.401233-05:00',
+      author: 1
+    },
+    {
+      id: 2,
+      title: 'Add Scheme',
+      detail: 'Add OpenAPI scheme to API.',
+      completed: false,
+      created_at: '2021-08-07T16:13:26.683593-05:00',
+      due_by: '2021-08-08T16:13:26.683608-05:00',
+      author: 2
+    },
+    {
+      id: 3,
+      title: 'Add API Documentation',
+      detail: 'Add Swagger API documentation to API.',
+      completed: false,
+      created_at: '2021-08-07T16:15:06.231946-05:00',
+      due_by: '2021-08-08T16:15:06.231964-05:00',
+      author: 2
+    }
+  ]);
+
   return (
     <div className="app-container container d-flex justify-content-center">
       <div className="card p-2 align-self-center">
         <Header />
-        <Tasks />
+        <Tasks tasks={tasks} />
       </div>
     </div>
   );
