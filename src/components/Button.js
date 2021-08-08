0bc0fd8543fa7fa93f441554c9bcaa6ac65e8384
mppatterson84 <mppatterson84@gmail.com>
2021-08-08 09:46:15 -0500

    add onClick prop types

--- src/components/Button.js
+++ src/components/Button.js
@@ -16,7 +16,8 @@ Button.defaultProps = {
 
 Button.propTypes = {
   text: PropTypes.string,
-  color: PropTypes.string
+  color: PropTypes.string,
+  onClick: PropTypes.func
 };
 
 export default Button;
