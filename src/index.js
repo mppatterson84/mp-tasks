c9d4a7b2ecf4c6410fbf213cc99c5938c9952915
mppatterson84 <mppatterson84@gmail.com>
2021-08-08 07:59:33 -0500

    add bootstrap imports to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/lib/bootstrap.custom.min.css';
+import './js/lib/bootstrap.bundle.min.js';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
