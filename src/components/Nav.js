a3b36051318cded9214a57ccc2bb5fa1d5b34e29
mppatterson84 <mppatterson84@gmail.com>
2021-08-14 11:39:47 -0500

    display conditional if user is signed in

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,32 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Nav = () => {
+const Nav = ({ username }) => {
+  const logout = async () => {
+    await fetch('http://127.0.0.1:8000/api/tasks/v1/rest-auth/logout/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include'
+    });
+  };
+
+  let menu;
+
+  if (username === '') {
+    menu = (
+      <ul className="navbar-nav me-auto mb-2 mb-md-8">
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Sign in
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/signup" className="nav-link">
+            Signup
+          </Link>
+        </li>
+      </ul>
+    );
+  } else {
+    menu = (
+      <ul className="navbar-nav me-auto mb-2 mb-md-8">
+        <li className="nav-item">
+          <Link to="/login" className="nav-link" onClick={logout}>
+            Sign out
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/profile" className="nav-link">
+            Profile
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           Tasks
         </Link>
-        <div>
-          <ul className="navbar-nav me-auto mb-2 mb-md-8">
-            <li className="nav-item">
-              <Link to="/login" className="nav-link">
-                Sign in
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/signup" className="nav-link">
-                Signup
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/profile" className="nav-link">
-                Profile
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <div>{menu}</div>
       </div>
     </nav>
   );
