561dfd6dc0caa9ad55eb136a9d6dd65042867bd5
mppatterson84 <mppatterson84@gmail.com>
2021-08-14 08:44:34 -0500

    update to signup new users to the API

--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,7 +1,74 @@
+import { useState } from 'react';
+import { Redirect } from 'react-router';
+
 const Signup = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password1, setPassword1] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    await fetch('http://127.0.0.1:8000/api/tasks/v1/rest-auth/registration/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username,
+        email,
+        password1,
+        password2
+      })
+    });
+
+    setRedirect(true);
+  };
+
+  if (redirect) {
+    return <Redirect to="/login" />;
+  }
+
   return (
-    <div>
-      <div>Signup</div>
+    <div className="card my-3 mx-auto p-2">
+      <form onSubmit={submit}>
+        <h3 className="mb-3 fw-normal">Create New User Account</h3>
+        <input
+          type="text"
+          placeholder="Username"
+          className="form-control mb-2"
+          name="username"
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="form-control mb-2"
+          name="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password1"
+          className="form-control mb-2"
+          name="password1"
+          required
+          onChange={(e) => setPassword1(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password2"
+          className="form-control mb-2"
+          name="password2"
+          required
+          onChange={(e) => setPassword2(e.target.value)}
+        />
+        <button type="submit" className="w-100 btn btn-primary">
+          Sign In
+        </button>
+      </form>
     </div>
   );
 };
