51ba8b524568ca28e153a3994728ab0a97a672cf
mppatterson84 <mppatterson84@gmail.com>
2021-08-14 11:38:23 -0500

    display conditional if user is signed in

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,7 +1,8 @@
-const Profile = () => {
+const Profile = ({ username }) => {
   return (
-    <div>
-      <div>Profile</div>
+    <div className="card my-3 mx-auto p-2">
+      <h3 className="mb-3 fw-normal">Profile</h3>
+      <p>{username ? `Hi, ${username}` : 'Please sign in.'}</p>
     </div>
   );
 };
