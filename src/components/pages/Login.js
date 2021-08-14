649e9482e2dd4fff97471a9d93e6b44d5065b433
mppatterson84 <mppatterson84@gmail.com>
2021-08-14 10:38:28 -0500

    update to log in users to the API

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,19 +1,48 @@
+import { useState } from 'react';
+import { Redirect } from 'react-router';
+
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    await fetch('http://127.0.0.1:8000/api/tasks/v1/rest-auth/login/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+      body: JSON.stringify({
+        username,
+        password
+      })
+    });
+
+    setRedirect(true);
+  };
+
+  if (redirect) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <div className="card my-3 mx-auto p-2">
-      <form>
+      <form onSubmit={submit}>
         <h3 className="mb-3 fw-normal">Sign In</h3>
         <input
           type="text"
           placeholder="Username"
           className="form-control mb-2"
           required
+          onChange={(e) => setUsername(e.target.value)}
         />
         <input
           type="password"
           placeholder="Password"
           className="form-control mb-2"
           required
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button type="submit" className="w-100 btn btn-primary">
           Sign In
